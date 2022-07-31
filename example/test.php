6397ba9fdc1aa12785afdbd9ddb6611e90781816
DeathSatan <2771717608@qq.com>
2022-07-31 14:10:15 +0800

    code init

---
+++ example/test.php
@@ -0,0 +1,3 @@
+<?php
+require_once dirname(__DIR__).'/vendor/autoload.php';
+var_dump(\Satan\Constant\Alias\DS);
---
+++ src/Alias.php
@@ -0,0 +1,7 @@
+<?php
+
+/**
+ * 目录分隔符
+ */
+define('Satan\\Constant\\Alias\\DS',DIRECTORY_SEPARATOR);
+define('Satan\\Constant\\Alias\\DATETIME',date('Y-m-d H:i:s'));
